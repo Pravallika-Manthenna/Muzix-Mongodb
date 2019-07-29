d8a8ae556372735782542b1d2aa3e78f6c76ea86
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-07-29 19:29:07 +0530

    Update WebConfig.java

--- src/main/java/com/stackroute/MuzixAppMysql/confiiguration/WebConfig.java
+++ src/main/java/com/stackroute/MuzixAppMysql/confiiguration/WebConfig.java
@@ -4,11 +4,13 @@ package com.stackroute.MuzixAppMysql.confiiguration;
  import org.springframework.context.annotation.Bean;
  import org.springframework.context.annotation.Configuration;
 
-
+//configuration annotation is used to identify the class as configuration class
 @Configuration
 public class WebConfig {
-
+ 
+ //creating bean using bean annotation
     @Bean
+  //using servlet registration bean which is inbuilt for bean creation
     public ServletRegistrationBean servletRegistrationBean() {
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new WebServlet());
         servletRegistrationBean.addUrlMappings("/console/*");
