76ea16cc5de78e1b4f7682fac493da68444e12d4
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-07-29 19:14:23 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/MuzixAppMysql/repository/TrackRepository.java
+++ src/main/java/com/stackroute/MuzixAppMysql/repository/TrackRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
+//repository annotation is used to identify that this is a repository
 @Repository
+//repository extends mongorepository because of mongodb usage
 public interface TrackRepository extends MongoRepository<Track,Integer> {
 
-//    @Query("SELECT t FROM Track t WHERE name = ?1")
+  //    @Query("SELECT t FROM Track t WHERE name = ?1")
 //    List<Track> getTrackByName(String name);
 //
 //    //@Query(value = "SELECT t FROM Track t WHERE (?1 is null OR name = ?1) AND (?2 = 0 OR id = ?2)")
