eeac5e5d2c63ad302141a67d78fb1558b99fcde1
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-07-29 19:17:49 +0530

    Update TrackNotFoundException.java

--- src/main/java/com/stackroute/MuzixAppMysql/exceptions/TrackNotFoundException.java
+++ src/main/java/com/stackroute/MuzixAppMysql/exceptions/TrackNotFoundException.java
@@ -1,11 +1,15 @@
 package com.stackroute.MuzixAppMysql.exceptions;
 
+//track not found exception extending exception
 public class TrackNotFoundException extends Exception {
 
+    //declaring string variable
     private String message;
 
+        //default constructor
     public TrackNotFoundException(){}
 
+    //constructor with string message parameter
     public TrackNotFoundException(String message) {
         super(message);
         this.message = message;
